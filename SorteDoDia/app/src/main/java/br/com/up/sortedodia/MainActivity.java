3b25745b7eb6eb338dfb9011fcbe9ee316dcfcbf
Gabriel Stenzowski <gabriel.stenzowskicav@gmail.com>
2022-08-19 20:59:23 -0300

    v0.02

--- SorteDoDia/app/src/main/java/br/com/up/sortedodia/MainActivity.java
+++ SorteDoDia/app/src/main/java/br/com/up/sortedodia/MainActivity.java
@@ -2,17 +2,24 @@ package br.com.up.sortedodia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewTittle;
     private Button buttonAction;
+    private ArrayList<String> messages = new ArrayList<>();
 
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +28,24 @@ public class MainActivity extends AppCompatActivity {
         textViewTittle = findViewById(R.id.text_tittle);
         buttonAction = findViewById(R.id.button_action);
 
-        textViewTittle.setText("Ola Mundão, Ele capota, não gira");
+        messages.add("Comprar uma coxinha");
+        messages.add("Dormi o maximo amanha");
+        messages.add("Fazer altos nada");
+        messages.add("Sexta feira é dia de maldade");
+
+//        textViewTittle.setText("Ola Mundo...");
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textViewTittle.setText("Ola Mundo");
+
+                Random random = new Random();
+                int index = random.nextInt(messages.size());
+                String message = messages.get(index);
+
+                textViewTittle.setText(message);
+
+//                textViewTittle.setText("Bom dia, são 7 da manha em malibu");
             }
         };
         buttonAction.setOnClickListener(listener);
